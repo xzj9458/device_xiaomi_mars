f96ef1eeef220a1af5ae1d59903e33e8211b919f
xzj9458 <xzj9458@gmail.com>
2024-01-17 14:55:54 +0800

    mars: Inherit sm8350-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,32 +9,3 @@ DEVICE_PATH := device/xiaomi/mars
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
-# Board
-TARGET_BOOTLOADER_BOARD_NAME := mars
-
-# Display
-TARGET_SCREEN_DENSITY := 560
-
-# Kernel
-TARGET_KERNEL_CONFIG += vendor/star_QGKI.config
-
-# Kernel modules
-BOOT_KERNEL_MODULES := \
-    fts_touch_spi.ko \
-    hwid.ko \
-    msm_drm.ko \
-    xiaomi_touch.ko
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
-
-# Partitions
-BOARD_DTBOIMG_PARTITION_SIZE := 25165824
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 114001162240
-
-# Properties
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
-
-# Recovery
-TARGET_RECOVERY_UI_MARGIN_HEIGHT := 165
-
-# Include proprietary files
-include vendor/xiaomi/mars/BoardConfigVendor.mk
